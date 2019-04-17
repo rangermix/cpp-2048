d067f1bb96941a865e52cc91afd2857291a10074
rangermix <fq_aaron@hotmail.com>
2019-04-17 21:45:41 +1000

    new method to update the board to support empty cells

--- src/StateTracker.h
+++ src/StateTracker.h
@@ -2,23 +2,56 @@
 #define STATE_TRACKER_H
 
 #include <vector>
+#include <tuple>
+#include <unordered_set>
+#include <functional>
 
 class StateTracker {
    private:
+    std::function<size_t (const std::tuple<int, int>&)> hash = [](const auto& t){
+        auto [i, j] = t;
+        auto h = std::hash<int>();
+        return h(10*i+j);
+    };
     std::vector<std::vector<int>> board;
+    std::unordered_set<std::tuple<int, int>, decltype(hash)> freeBlocks;
     int size;
 
-    void generateBlock() { board[0][0] = 2; }
+    // TODO: implement findEmpty
+    std::tuple<int, int> findEmpty() {
+        // int n = freeBlocks.size();
+
+        return std::make_tuple(0, 0);
+    }
+
+    void generateBlock() {
+        auto [i, j] = findEmpty();
+        update(i, j, 2);
+    }
+
+    void update(int i, int j, int val) {
+        board[i][j] = val;
+        if (val==0) {
+            freeBlocks.emplace(i, j);
+        } else {
+            freeBlocks.erase(std::make_tuple(i, j));
+        }
+    }
 
    public:
     enum Direction { UP, RIGHT, DOWN, LEFT };
 
     StateTracker(int n = 4)
         : board(std::vector<std::vector<int>>(n, std::vector<int>(n))),
+          freeBlocks(std::unordered_set<std::tuple<int, int>, decltype(hash)>(16, hash)),
           size(n) {
-        board[1][0] = 2;
-        board[3][2] = 2;
-        board[2][3] = 2;
+        for (int i=0; i<n; ++i) {
+            for (int j=0; j<n; ++j) {
+                freeBlocks.emplace(i, j);
+            }
+        }
+        generateBlock();
+        generateBlock();
     }
 
     void move(Direction drc) {
@@ -43,10 +76,12 @@ class StateTracker {
             while (pnext < size && pnext >= 0) {
                 if (board[row_next][col_next] != 0) {
                     if (board[row_curr][col_curr] == 0) {
-                        std::swap(board[row_next][col_next], board[row_curr][col_curr]);
+                        int t = board[row_next][col_next];
+                        update(row_next, col_next, board[row_curr][col_curr]);
+                        update(row_curr, col_curr, t);
                     } else if (board[row_next][col_next] == board[row_curr][col_curr]) {
-                        board[row_curr][col_curr] += board[row_next][col_next];
-                        board[row_next][col_next] = 0;
+                        update(row_curr, col_curr, board[row_curr][col_curr]*2);
+                        update(row_next, col_next, 0);
                         pcurr += delta;
                     } else if (p1-p2>1) {
                         pcurr += delta;
